f94375039e85158b20dadf5cc06c8a5e9c65db85
aburakuslu <alpburakuslu@gmail.com>
2023-05-29 00:59:01 +0300

    day03

---
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -0,0 +1,18 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class HerOkuAppBaseUrl {
+    //setUp() method'u ile request işleminde tekrarlı şekilde yapılacak işlemler burada bir kez yapılacak.
+    // @Test öncesi çalışması için @Before anotasyonu ekliyoruz.
+
+    protected RequestSpecification spec;
+
+    @Before //Her @Test method'u öncesi çalışır
+    public void setUp() {
+        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com/").build();
+    }
+}
