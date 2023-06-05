2c0c12a1dc759c736931a03e615b18db1b9202ad
aburakuslu <alpburakuslu@gmail.com>
2023-06-05 23:56:35 +0300

    day08

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -8,15 +8,24 @@ public class JsonPlaceHolderTestData {
     public Map<String, Object> expectedDataMapMethod(Integer userId, String title, Boolean completed) {
 
         Map<String, Object> expectedData = new HashMap<>();
-        if(userId != null) {
+        if(userId != null){
             expectedData.put("userId", userId);
         }
-        if(title != null) {
+        if(title!=null){
             expectedData.put("title", title);
         }
-        if(completed != null) {
+        if (completed != null){
             expectedData.put("completed", completed);
         }
         return expectedData;
+    }
+
+    public static String expectedDataInString(int userId, String title, boolean completed){
+
+        return "{\n" +
+                "\"userId\": "+userId+",\n" +
+                "\"title\": \""+title+"\",\n" +
+                "\"completed\": "+completed+"\n" +
+                "}";
     }
 }
