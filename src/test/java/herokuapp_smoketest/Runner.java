35a0c23bb5c60d64b86323db50fc6bcfca927e1a
aburakuslu <alpburakuslu@gmail.com>
2023-07-12 22:06:41 +0300

    day09 v1

---
+++ src/test/java/herokuapp_smoketest/Runner.java
@@ -0,0 +1,19 @@
+package herokuapp_smoketest;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                C01_PostRequest.class,
+                C02_GetRequest.class
+
+        }
+
+
+)
+//Oluşturulan classların çalışma sırasını belirtiyoruz
+public class Runner  {
+}
